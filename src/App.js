ef9a19bcb7a396ed09a3531f09581e7b865222f6
Thais <84157862+LennyBla@users.noreply.github.com>
2024-03-20 21:38:44 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
+//import Pages from './pages'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-
+<h1>OLA</h1>
       </header>
     </div>
   );
