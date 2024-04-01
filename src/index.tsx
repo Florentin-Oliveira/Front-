5c82c5ba7e6c7a9fc0ccd0c95d57f5dc38624c7b
LennyBla <thais.florentin26@gmail.com>
2024-04-01 16:34:28 -0300

    Migração para o Typescript

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Home from './pages/Home.jsx';
+import App from './App.jsx';
+
+ReactDom.render(
+  <React.StrictMode>
+    {/*<App />*/}
+    <Home />
+  </React.StrictMode>
+)
