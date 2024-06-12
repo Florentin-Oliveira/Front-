c378e955460ed9d85ce1b4fcacb7591bbd339137
LennyBla <thais.florentin26@gmail.com>
2024-06-12 15:38:33 -0300

    \Correcao

--- src/paginas/Home.tsx
+++ src/paginas/Home.tsx
@@ -3,11 +3,10 @@ import Home from "./Home";
 import Botao from "../components/Botao/index";
 import Cliente from "../components/Cliente/Cliente";
 import OS from "../components/OS/OrdenServico";
-import style from './App.module.scss';
 
 function App() {
   return (
-    <div className={style.AppStyle}>
+    <div>
       <Home />
       <Cliente />
       <Botao />
